173a0c6b8a6f88a111e8ed64a71befb067748b6e
Aleh <blissful.carbon@gmail.com>
2021-05-09 12:55:48 +0300

    Register input service

--- Assets/CodeBase/Infrastructure/Game.cs
+++ Assets/CodeBase/Infrastructure/Game.cs
@@ -1,7 +1,23 @@
-﻿namespace CodeBase.Infrastructure
+﻿using CodeBase.Services.Inputs;
+using UnityEngine;
+
+namespace CodeBase.Infrastructure
 {
     public class Game
     {
-    
+        public IInputService InputService;
+        
+        public  Game()
+        {
+            RegisterInputService();
+        }
+
+        private void RegisterInputService()
+        {
+            if (Application.isEditor)
+                InputService = new StandaloneInputServiceImpl();
+            else
+                InputService = new MobileInputServiceImpl();
+        }
     }
 }
