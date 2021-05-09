629396b39c990082029801a0c28ee8b1620d06e9
Aleh <blissful.carbon@gmail.com>
2021-05-09 12:45:48 +0300

    Mobile implementation IInputService

---
+++ Assets/CodeBase/Services/Inputs/MobileInputServiceImpl.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace CodeBase.Services.Inputs
+{
+    public class MobileInputServiceImpl : InputService
+    {
+        public override Vector2 Axis => GetSimpleInputAxis();
+    }
+}
