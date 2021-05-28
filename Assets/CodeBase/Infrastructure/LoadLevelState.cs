e8c5b47e7e9c1931ba61c5abb0b0c9a4e9d9f63c
Aleh <blissful.carbon@gmail.com>
2021-05-29 00:10:00 +0300

    Add scene loader

---
+++ Assets/CodeBase/Infrastructure/LoadLevelState.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CodeBase.Infrastructure.Main
+{
+    public class LoadLevelState : IState
+    {
+        private const string SceneNameDevelop = "Develop";
+        private GameStateMachine _gameStateMachine;
+        private SceneLoader _sceneLoader;
+        
+        public LoadLevelState(GameStateMachine gameStateMachine, SceneLoader sceneLoader)
+        {
+            _gameStateMachine = gameStateMachine;
+            _sceneLoader = sceneLoader;
+        }
+
+        public void Enter()
+        {
+            _sceneLoader.Load(SceneNameDevelop);
+        }
+
+        public void Exit()
+        {
+            
+        }
+    }
+}
